e8340213347c5cc287a8196e7b64dc38c4a12e53
Tom <tryhardgaminguk@googlemail.com>
2020-06-17 20:24:06 +0100

    Update Build Script

---
+++ build/precache-manifest.b1e740ce18eea83a742b630d301e2e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8708bbca6bf32f72421fb958d0de7260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb3702ed85d078233fd",
+    "url": "/static/css/2.9f09efc6.chunk.css"
+  },
+  {
+    "revision": "4816b5c3703fd7c62e78",
+    "url": "/static/css/main.e8d41f5d.chunk.css"
+  },
+  {
+    "revision": "5cb3702ed85d078233fd",
+    "url": "/static/js/2.e6b335c4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e6b335c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4816b5c3703fd7c62e78",
+    "url": "/static/js/main.ee3a8c75.chunk.js"
+  },
+  {
+    "revision": "192a71dc801613e28029",
+    "url": "/static/js/runtime-main.67745738.js"
+  }
+]);
