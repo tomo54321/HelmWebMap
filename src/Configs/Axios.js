bb069b5d81419358668101aac9690bf3be505553
Tom <tryhardgaminguk@googlemail.com>
2021-03-30 20:12:17 +0100

    Update Axios.js

--- src/Configs/Axios.js
+++ src/Configs/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://currentproject.test" : "https://helmapp.net") + "/api/v1/"
+export const baseURL = ((!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://currentproject.test" : "https://helmapp.co.uk") + "/api/v1/"
 
 const instance = axios.create({
   baseURL,
